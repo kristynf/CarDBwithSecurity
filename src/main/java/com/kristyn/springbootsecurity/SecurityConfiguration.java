5827a8a1dbe742f3c720da72b8737227d61b0269
kristynf <kristynf@bu.edu>
2020-03-02 15:33:03 -0500

    updated

--- src/main/java/com/kristyn/springbootsecurity/SecurityConfiguration.java
+++ src/main/java/com/kristyn/springbootsecurity/SecurityConfiguration.java
@@ -34,7 +34,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception{
         http
                 .authorizeRequests()
-                .antMatchers("/", "/h2-console/**", "/register").permitAll()
+                .antMatchers("/", "/h2-console/**", "/register", "/css/**").permitAll()
                 .antMatchers("/admin").access("hasAnyAuthority('ADMIN')")
                 .anyRequest().authenticated()
                 .and().formLogin().loginPage("/login").permitAll()
